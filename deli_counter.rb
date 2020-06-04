adbb5ff57c4637517b7d3b4de04b52d845cd3ab0
Angela Stephens <angelastephens317@gmail.com>
2020-06-04 02:24:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line (line_array)
   if line_array.size == 0
-    puts " The line is currently empty."
+    puts "The line is currently empty."
   else
     name_array = line_array.map.with_index(1) do 
       |name,index| "#{index}. #{name}"
